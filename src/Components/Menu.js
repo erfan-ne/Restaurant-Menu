78685034da26bc8c7c19a9ddf9120681219259e4
erfan-ne <erfan.neshati@gmail.com>
2025-11-16 19:55:46 +0330

    update App.css

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -3,25 +3,23 @@ import menus from "../data";
 
 const Menu = () => {
   return (
-    <>
-        <div className="section-center">
+    <div className="section-center">
       {menus.map((menu) => (
-          <article className="menu-item" key={menu.id}>
-            <img src={menu.img} className="photo" />
-            <div className="item-info">
-              <header>
-                <h4>{menu.title}</h4>
-                <h4 className="price">${menu.price}</h4>
-              </header>
-              <p className="item-text">{menu.desc}</p>
-              <footer>
-                <span>Category: {menu.category}</span>
-              </footer>
-            </div>
-          </article>
+        <article className="menu-item" key={menu.id}>
+          <img src={menu.img} className="photo" />
+          <div className="item-info">
+            <header>
+              <h4>{menu.title}</h4>
+              <h4 className="price">${menu.price}</h4>
+            </header>
+            <p className="item-text">{menu.desc}</p>
+            <footer>
+              <span>Category: {menu.category}</span>
+            </footer>
+          </div>
+        </article>
       ))}
-        </div>
-    </>
+    </div>
   );
 };
 
