89fb22bb2c85be256c33fda4aeafc69cabbd745b
erfan-ne <erfan.neshati@gmail.com>
2025-11-16 20:58:29 +0330

    update Categories.js

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,27 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import menus from "../data"
 
 const Categories = () => {
 
-  const mainCategories = [...new Set(menus.map(menu => menu.category))];
-  const [selectCategory, setSelectCategory] = useState("")
-
-  useEffect( () => {console.log(selectCategory)} , [selectCategory])
-
+  const mainCategories = ["All", ...new Set(menus.map(menu => menu.category))];
+  const [selectCategory, setSelectCategory] = useState("All")
 
   return (
     <div className="btn-container">
-      <button
-        type="button"
-        className="filter-btn"
-        onClick={() => setSelectCategory("All")}
-      >
-        All
-      </button>
       {mainCategories.map(mainCategory => 
         <button key={mainCategory}
         type="button"
-        className={selectCategory === mainCategory ? "filter-btn highlight" : "filter-btn"}
+        className={`filter-btn ${selectCategory === mainCategory ? "highlight" : ""}`}
         onClick={() => setSelectCategory(mainCategory)}
       >
         {mainCategory}
